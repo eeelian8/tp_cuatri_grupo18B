50fb78ae523d116781875a8de7847d01513d0ce4
Kat.Lover <jgumiela7@gmail.com>
2024-11-21 21:58:10 -0300

    contenido agregado a asignarTrabajo con calendario y repeater

--- AppSeguridad/AsignarTecnico.aspx
+++ AppSeguridad/AsignarTecnico.aspx
@@ -1,5 +1,41 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="AsignarTecnico.aspx.cs" Inherits="AppSeguridad.AsignarTecnico" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container mt-3">
+        <%-- Calendario miausis --%>
+        <div class="row mb-3">
+            <div class="col">
+                <asp:Calendar ID="calFecha" runat="server" BackColor="White" 
+                    OnSelectionChanged="calFecha_SelectionChanged" 
+                    OnDayRender="calFecha_DayRender">
+                </asp:Calendar>
+            </div>
+        </div>
+
+        <%--repetidor tecnicos --%>
+        <div class="row mb-3">
+            <div class="col">
+                <asp:Repeater ID="repTecnicos" runat="server" OnItemDataBound="repTecnicos_ItemDataBound">
+                    <ItemTemplate>
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <asp:RadioButton ID="rbTecnico" runat="server" GroupName="tecnicos" />
+                                <asp:Label ID="lblNombreTecnico" runat="server"></asp:Label>
+                            </div>
+                        </div>
+                    </ItemTemplate>
+                </asp:Repeater>
+            </div>
+        </div>
+
+        <%-- btn asignar --%>
+        <div class="row">
+            <div class="col">
+                <asp:Button ID="btnAsignar" runat="server" Text="Asignar Técnico" 
+                    CssClass="btn btn-primary" OnClick="btnAsignar_Click" />
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- AppSeguridad/AsignarTecnico.aspx.cs
+++ AppSeguridad/AsignarTecnico.aspx.cs
@@ -5,13 +5,77 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Dominio;
+using Negocio;
+
 namespace AppSeguridad
 {
     public partial class AsignarTecnico : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                calFecha.SelectedDate = DateTime.Today; //solo fecha desde hoy
+                
+                CargarTecnicosDisponibles();
+            }
         }
+
+        protected void calFecha_DayRender(object sender, DayRenderEventArgs e)
+        {
+            // Deshabilitar fechas pasadas
+            if (e.Day.Date < DateTime.Today)
+            {
+                e.Day.IsSelectable = false;
+                e.Cell.ForeColor = System.Drawing.Color.Gray;
+            }
+        }
+
+        protected void calFecha_SelectionChanged(object sender, EventArgs e)
+        {
+            CargarTecnicosDisponibles(); 
+        }
+
+        private void CargarTecnicosDisponibles()
+        {
+            try
+            {
+                TecnicoNegocio negocio = new TecnicoNegocio();
+                repTecnicos.DataSource = negocio.ListarTecnicosDisponibles(calFecha.SelectedDate);
+                repTecnicos.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void btnAsignar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void repTecnicos_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            try
+            {
+                if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+                {
+                    dynamic dataItem = e.Item.DataItem;
+                    RadioButton rb = (RadioButton)e.Item.FindControl("rbTecnico");
+                    Label lbl = (Label)e.Item.FindControl("lblNombreTecnico");
+
+                    lbl.Text = dataItem.Nombre + " " + dataItem.Apellido + " - " + dataItem.Localidad;
+                    rb.ID = dataItem.CodTecnico;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error carga de técnico: " + ex.Message);
+            }
+        }
+
     }
+
 }
